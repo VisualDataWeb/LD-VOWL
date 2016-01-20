24a4bc20ea324d4d343879c97d5575c3684085d0
Marc Weise <weisemc@studi.informatik.uni-stuttgart.de>
2016-01-20 22:39:10 +0000

    Fix broken detail section due to missing URI label filter.

---
+++ app/filters/uri-label.js
@@ -0,0 +1,11 @@
+module.exports = function () {
+  return function (uri) {
+    var uriLabel = '';
+    if (uri !== undefined && uri !== '') {
+      var lastSlash = uri.lastIndexOf('/');
+      var lastHash = uri.lastIndexOf('#');
+      uriLabel = uri.substr(Math.max(lastSlash, lastHash) + 1).replace(/\_/g, ' ');
+    }
+    return uriLabel;
+  };
+};
