32c9e84c24609494ea06c870f418fd266dcb77b5
Marc Weise <weisemc@studi.informatik.uni-stuttgart.de>
2016-02-10 20:52:25 +0000

    Clean up query factory.
    - Remove old queries from test application
    - Remove prefixes never used

--- app/services/requests/query-factory.srv.js
+++ app/services/requests/query-factory.srv.js
@@ -3,16 +3,13 @@
 module.exports = function () {
 
     var namespaces = [
-        'owl: <http://www.w3.org/2002/07/owl#>',
-        'rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>',
-        'rdfs: <http://www.w3.org/2000/01/rdf-schema#>',
-        'xml: <http://www.w3.org/XML/1998/namespace>'
+      'rdfs: <http://www.w3.org/2000/01/rdf-schema#>',
+      'skos: <http://www.w3.org/2004/02/skos/core#>'
     ];
 
     var defaultLimit = 10;
     var defaultOffset = 0;
     var defaultLang = 'en';
-    var defaultBaseClass = 'owl:Thing';
 
     var prefixes = function () {
       var pre = '';
@@ -56,42 +53,11 @@ module.exports = function () {
         return query;
       },
 
-      getSubClassQuery: function (baseClass, labelLang, limit, offset) {
-
-        // check parameters
-        baseClass = (typeof baseClass === 'string') ? baseClass : defaultBaseClass;
-        labelLang = (typeof labelLang === 'string') ? labelLang : defaultLang;
-        limit = (typeof limit === 'number' && limit > 0) ? limit : defaultLimit;
-        offset = (typeof offset === 'number' && offset >= 0) ? offset :defaultOffset;
-
-        var query = prefixes() +
-          "SELECT ?class (SAMPLE (?lbl) AS ?label) (count(DISTINCT ?subClass) AS ?count) " +
-          "WHERE { " +
-            "?class rdfs:subClassOf+ <" + baseClass + ">. " +
-            "?subClass rdfs:subClassOf+ ?class. " +
-            "?class rdfs:label ?lbl. " +
-            "FILTER (langMatches(lang(?lbl),'" + labelLang + "')) " +
-          "} " +
-          "GROUP BY ?class ORDER BY DESC (?count) " +
-          "LIMIT " + limit + " OFFSET " + offset;
-          return query;
-      },
-
-      getSuperClassQuery: function (childClassURI, labelLang) {
-        var query = prefixes() +
-          "SELECT ?superClass (SAMPLE (?lbl) AS ?superClassLabel) " +
-          "WHERE { " +
-            "<" + childClassURI + "> rdfs:subClassOf  ?superClass. " +
-            "?superClass rdfs:label ?lbl . " +
-            "FILTER (langMatches(lang(?lbl),'" + labelLang + "')) " +
-          "}";
-
-        return query;
-      },
-
       // PROPERTY QUERIES
 
       getLabelQuery: function (uri, labelLang) {
+        labelLang = labelLang || defaultLang;
+
         var query = prefixes() +
         "SELECT (SAMPLE (?lbl) AS ?label) " +
         "WHERE { " +
@@ -102,43 +68,15 @@ module.exports = function () {
       },
 
       getPreferredLabelQuery: function (uri, labelLang) {
+        labelLang = labelLang || defaultLang;
+
         var query = prefixes() +
-            "PREFIX skos: <http://www.w3.org/2004/02/skos/core#> " +
             "SELECT ?label " +
             "WHERE { " +
               "<" + uri + "> skos:prefLabel ?label . " +
               "FILTER (langMatches(lang(?label), '" + labelLang + "')) " +
             "}";
         return query;
-      },
-
-      // TODO improve and use this one!
-      getTopProperties: function (classURI) {
-        var query = prefixes() +
-          "SELECT DISTINCT ?prop (count(?instance) as ?count) " +
-          "WHERE { " +
-            "?instance a <" + classURI + ">. " +
-            "?instance ?prop ?obj. " +
-          " } " +
-          "ORDER BY DESC(?count) " +
-          "LIMIT 10";
-          return query;
-      },
-
-      getReferringTypes: function (classURI, limit) {
-        limit = (typeof limit === 'number' && limit > 0) ? limit : defaultLimit;
-
-        var typeQuery = prefixes() +
-          "SELECT ?valType (COUNT(?valType) AS ?count) " +
-          "WHERE { " +
-            "<" + classURI + "> ?prop ?val . " +
-            "BIND (datatype(?val) AS ?valType) . " +
-          "} " +
-          "GROUP BY ?valType " +
-          "ORDER BY ?count " +
-          "LIMIT " + limit;
-
-        return typeQuery;
       },
 
       getInstanceReferringTypesQuery: function (classURI, limit) {
@@ -154,98 +92,6 @@ module.exports = function () {
           "GROUP BY ?valType " +
           "LIMIT " + limit;
         return typeQuery;
-      },
-
-      getPropertyQuery: function (propertyType, domainClass, labelLang, limit) {
-        var query = prefixes() +
-          "SELECT ?property (SAMPLE (?lbl) AS ?label ) " +
-          "WHERE { ?property a owl:" +  propertyType + ". " +
-            "?property rdfs:domain " + domainClass + ". " +
-            "?property rdfs:label ?lbl. " +
-            "?property rdfs:range ?range. " +
-            "FILTER langMatches(lang(?lbl), '" + labelLang + "') " +
-          "} " +
-          "GROUP BY ?property " +
-          "ORDER BY ?property " +
-          "LIMIT " + limit;
-        return query;
-      },
-
-      getSubPropertyQuery: function (baseProperty, limit) {
-        var query = prefixes() +
-          "SELECT ?subProp (count(DISTINCT ?subProp) AS ?count) " +
-          "WHERE { " +
-            "?subProp rdfs:subPropertyOf+ <" + baseProperty + "> . " +
-          "} " +
-          "GROUP BY ?subProp " +
-          "ORDER BY DESC (?count) " +
-          "LIMIT " + limit;
-        return query;
-      },
-
-      getSuperPropertyQuery: function (propertyURI) {
-        var query = prefixes() +
-          "SELECT ?superProp " +
-          "WHERE { " +
-            "<" + propertyURI + "> rdfs:subPropertyOf ?superProp. " +
-          "}";
-        return query;
-      },
-
-      // TODO also include childs of equivalent classes
-      getChildCountQuery: function (classURI) {
-        var query = prefixes() +
-          "SELECT (count(?subClass) AS ?count) " +
-          "WHERE { " +
-            "?subClass rdfs:subClassOf <" + classURI + ">. " +
-          "}";
-        return query;
-      },
-
-      /**
-       * Returns a SPARQL query for fetching all properties of the class with the given URI.
-       * Uses rdfs:domain, this is really fast but does not work for every class.
-       */
-      getPropertyCountQuery: function (classURI) {
-        var query = prefixes() +
-          'SELECT (count(DISTINCT ?property) AS ?propertyCount) ' +
-          'WHERE { ' +
-            '{ ' +
-              '?property rdfs:domain ?class. ' +                // property belongs to a class, ...
-              '<' + classURI + '> rdfs:subClassOf+ ?class. ' +  // given class is sub class of this
-            '} UNION { ' +                                      // OR:
-              '?property rdfs:domain <' + classURI + '>. ' +    // property belongs to given class
-            '} ' +
-          '}';
-        return query;
-      },
-
-      /**
-       * More generic solution to get all the properties of a class, but much too complicated,
-       * doesn't work on public endpoints like dbpedia because of timeout.
-       */
-      // getPropertyCountQuery: function (classURI) {
-      //   var query = prefixes() +
-      //     'SELECT (count(DISTINCT ?property) AS ?propertyCount) ' +
-      //     'WHERE { ' +
-      //       '?sub a <' +classURI +'>. ' +
-      //       '?sub ?property ?obj. ' +
-      //     '}';
-      //   return query;
-      // },
-
-      /**
-       * Query for range of a given property. Range label is NOT included, because some ranges have
-       * no label and would be not be fetched otherwise.
-       */
-      getPropertyRangeQuery: function (propertyURI) {
-        var query = prefixes() +
-          'SELECT ?range ' +
-          'WHERE { ' +
-            '<' + propertyURI + '> rdfs:range ?range. ' +
-          '}' +
-          "GROUP BY ?range ";
-          return query;
       },
 
       // RELATION queries
@@ -273,7 +119,7 @@ module.exports = function () {
             "?originInstance a <" + originClass + "> . " +
             "?targetInstance a <" + targetClass + "> . " +
             "?originInstance ?prop ?targetInstance . " +
-          "}" +
+          "} " +
           "LIMIT " + limit + " " +
           "OFFSET " + offset;
         return query;
@@ -293,15 +139,6 @@ module.exports = function () {
       },
 
       // INSTANCE QUERIES
-
-      getInstanceCountQuery: function (classURI) {
-        var query = prefixes() +
-          'SELECT (count(?instance) AS ?instanceCount) ' +
-          'WHERE { ' +
-          '?instance a <' + classURI + '>. ' +
-          '}';
-        return query;
-      },
 
       getNumberOfCommonInstancesQuery: function (classURI1, classURI2) {
         var query = prefixes() +
@@ -325,5 +162,6 @@ module.exports = function () {
           return commentQuery;
       }
 
-    };
-  };
+    }; // end of public API to return
+
+  }; // end of module.exports
